c78a9d176c31dbd511dd4aa4564ced98f51c0287
Aindrila <aindrila.saha.kgec@gmail.com>
2022-04-05 22:38:28 +0530

    added api url

--- app.py
+++ app.py
@@ -10,14 +10,19 @@ app = Flask(__name__)
 @app.get("/")
 def receive():
     # image_url = "http://127.0.0.1:5001/"
-    image_url = "https://mlp-web-service.herokuapp.com/"
-    response = requests.get(image_url)
-    img = response.content
+    image_url_1 = "https://mlp-web-service.herokuapp.com/"
+    response_1 = requests.get(image_url_1)
+    img_1 = response_1.content
+    buffered_1 = io.BytesIO(img_1)
+    img_url_1 = base64.b64encode(buffered_1.getvalue()).decode()
     
-    buffered = io.BytesIO(img)
-    img_url = base64.b64encode(buffered.getvalue()).decode()
+    image_url_2 = "https://lr-web-service.herokuapp.com/"
+    response_2 = requests.get(image_url_2)
+    img_2 = response_2.content
+    buffered_2 = io.BytesIO(img_2)
+    img_url_2 = base64.b64encode(buffered_2.getvalue()).decode()
     
-    return render_template("home.html",images={ 'image': img_url })
+    return render_template("home.html",images={ 'image_1': img_url_1,'image_2': img_url_2 })
 
 if __name__ == "__main__":
     app.run(debug=True)
